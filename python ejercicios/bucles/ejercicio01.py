d09d15e94464c2a656597a1e81d2c9ec90265c51
David <140851329+11julio11@users.noreply.github.com>
2024-03-22 18:41:16 -0500

    121334

--- python ejercicios/bucles/ejercicio01.py
+++ python ejercicios/bucles/ejercicio01.py
@@ -3,11 +3,12 @@ Escribir un programa que pida al usuario una palabra y
  la muestre por pantalla 10 veces.
 '''
  # Solicita al usuario que ingrese una palabra
- palabra = input("Introduce una palabra: ")
+palabra = input("Introduce una palabra: ")
 
  #solicitamos la cantidad de veces que se repite la palabra
- repeticiones = int(input("intruce la cantidad de veces que se repite la palabra: "))
+cantidad = int(input("intruce la cantidad de veces que se repite la palabra: "))
 
  #imprimimos por pantalla la palabra introducida
-for i in range(repeticiones):
-    print(palabra)
+for i in range(cantidad):
+    print("Palabras repetidas:", palabra)
+
