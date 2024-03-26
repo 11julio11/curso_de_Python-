1604002330568fe91e8c22dac42768f8aa6be72f
David <140851329+11julio11@users.noreply.github.com>
2024-03-26 17:59:28 -0500

    espacio

--- python ejercicios/bucles/ejercicio01.py
+++ python ejercicios/bucles/ejercicio01.py
@@ -5,10 +5,13 @@ Escribir un programa que pida al usuario una palabra y
  # Solicita al usuario que ingrese una palabra
 palabra = input("Introduce una palabra: ")
 
+
  #solicitamos la cantidad de veces que se repite la palabra
+
 cantidad = int(input("intruce la cantidad de veces que se repite la palabra: "))
 
  #imprimimos por pantalla la palabra introducida
+
 for i in range(cantidad):
     print("Palabras repetidas:", palabra * cantidad)
 
